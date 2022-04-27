345c555f93deba0808113821aa2db59dba4021ba
mmarienko <maksym.marienko@gmail.com>
2022-04-27 03:51:40 +0300

    styles, components, props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import Car from './Car/Car';
 
 function App() {
+  const divStyle = {
+    textAlign: 'center'
+  };
+
   return (
-    <div className="App">
+    <div style={divStyle}>
       <h1>Hello world!</h1>
+
+      <Car name={"Ford"} year={"2018"}>
+       <p style={{color: 'blue'}}>COLOR</p>
+      </Car>
+      <Car name="Audi" year="2016">
+        <p style={{color: 'red'}}>COLOR</p>
+      </Car>
+      <Car name="Mazda" year="2010"/> 
     </div>
   );
 }
